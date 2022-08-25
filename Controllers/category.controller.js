e4264de2f1ec8c4d6652fd0c5bc3957eee3f7645
thangbui258 <thangbuimanh1996@gmail.com>
2022-08-25 16:52:37 +0700

    updateAPI BE add,delete,edit category

--- Controllers/category.controller.js
+++ Controllers/category.controller.js
@@ -1,9 +1,57 @@
 const Category = require('../Models/category.model')
 
 module.exports = {
-    showListCategory: async (req,res) => {
+    showListCategory: async (req, res) => {
         data = await Category.find()
-        res.status(200).json({success:true,data})
+        res.status(200).json({success: true, data})
+    },
+
+    addCategory: async (req, res) => {
+        const {type, name, icon} = req.body;
+        if (!type || !name || !icon)
+            return res.status(400).json({success: false, message: 'invalid'})
+        try {
+            const newCategory = new Category({
+                type,
+                name,
+                icon
+            })
+            await newCategory.save()
+            res.status(200).json({success: true, message: 'them moi thanh cong'})
+        } catch (err) {
+            console.log(err)
+            res.status(401).json({success: false, message: 'them khong thanh cong'})
+        }
+    },
+
+    updateCategory: async (req, res) => {
+        const {type, name, icon} = req.body;
+        if (!type || !name || !icon)
+            return res.status(400).json({success: false, message: 'invalid'})
+        try {
+            await Category.updateOne({_id: req.params.id}, {
+                type: type,
+                name: name,
+                icon: icon
+            })
+            res.status(200).json({success: true, message: 'update thanh cong'})
+        } catch (err) {
+            console.log(err)
+            res.status(401).json({success: false, message: 'update khong thanh cong'})
+        }
+    },
+
+    deleteCategory: async (req,res) => {
+        const deleteId = req.params.id;
+        if (!deleteId)
+            return res.status(400).json({succes: false, message: 'khong ton tai category'});
+        try {
+            await Category.deleteOne({_id:deleteId})
+            res.status(200).json({success: true, message: 'xoa thanh cong'})
+        } catch (err) {
+            console.log(err)
+            res.status(401).json({success: false, message: 'xoa khong thanh cong'})
+        }
     }
 
 
