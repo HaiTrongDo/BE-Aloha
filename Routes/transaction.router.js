14ce4df9b23ae040a6e534ffcc6386ba1b48e182
NQT <trungngxx@gmail.com>
2022-08-25 16:45:31 +0700

    transaction

--- Routes/transaction.router.js
+++ Routes/transaction.router.js
@@ -5,5 +5,7 @@ const transactionController=require('../Controllers/transaction.controller')
 router.post('/add',transactionController.addTransaction)
 router.get('/',transactionController.renderTransaction)
 router.get('/category',transactionController.renderCategory)
+router.get('/category/expense',transactionController.listExpense)
+router.get('/category/income',transactionController.listIncome)
 
 module.exports=router;
