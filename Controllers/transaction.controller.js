828943fac425643d99d04986846d2ead724e2a9f
NQT <trungngxx@gmail.com>
2022-08-28 15:08:28 +0700

    update delete transaction

--- Controllers/transaction.controller.js
+++ Controllers/transaction.controller.js
@@ -2,55 +2,63 @@ const Transaction = require('../Models/transaction.model')
 const Icon = require('../Models/icon.model');
 const Category = require('../Models/category.model')
 
-module.exports={
-    addTransaction: async(req,res,next)=>{
-        const transaction= new Transaction({
-            wallet:req.body.wallet,
-            amount:req.body.amount,
-            category:req.body.category,
-            date:req.body.date,
-            note:req.body.note,
-            user:req.body.user
+module.exports = {
+    addTransaction: async (req, res, next) => {
+        const transaction = new Transaction({
+            wallet: req.body.wallet,
+            amount: req.body.amount,
+            category: req.body.category,
+            date: req.body.date,
+            note: req.body.note,
+            user: req.body.user
         })
         await transaction.save(err => {
-            if(err){
+            if (err) {
                 throw err
             }
 
-            res.status(200).json({success:true,data:transaction})
+            res.status(200).json({success: true, data: transaction})
         })
     },
-    listTransaction: async (req,res,next)=>{
+    listTransaction: async (req, res, next) => {
         const transaction = await Transaction.find()
-        res.json({success:true,data:transaction})
+        res.json({success: true, data: transaction})
     },
-    listTransactionUser: async (req,res,next)=>{
-      const list = await Transaction.find({user:req.body.user}).populate('category')
-        res.json({success:true,data:list})
+    listTransactionUser: async (req, res, next) => {
+        const list = await Transaction.find({user: req.body.user}).populate([{path: 'category'}, {
+            path: 'wallet',
+            populate: {path: 'icon'}
+        }])
+        res.json({success: true, data: list})
     },
-    listCategory: async (req,res,next)=>{
+    listCategory: async (req, res, next) => {
         const category = await Category.find();
-        res.json({success:true,data:category})
+        res.json({success: true, data: category})
     },
-    listExpense: async(req,res,nex)=>{
-        const category = await Category.find({type:'EXPENSE'})
-        res.json({success:true,data:category})
+    listExpense: async (req, res, nex) => {
+        const category = await Category.find({type: 'EXPENSE'})
+        res.json({success: true, data: category})
     },
-    listIncome: async (req,res,next)=>{
-        const category = await Category.find({type:'INCOME'})
-        res.json({success:true,data:category})
+    listIncome: async (req, res, next) => {
+        const category = await Category.find({type: 'INCOME'})
+        res.json({success: true, data: category})
     },
-    editTransaction:async (req,res,next)=>{
-        const transaction= {
-            wallet:req.body.wallet,
-            amount:req.body.amount,
-            category:req.body.category,
-            date:req.body.date,
-            note:req.body.note,
-            user:req.body.user
+    editTransaction: async (req, res, next) => {
+        const transaction = {
+            wallet: req.body.wallet,
+            amount: req.body.amount,
+            category: req.body.category,
+            date: req.body.date,
+            note: req.body.note,
+            user: req.body.user
         }
-        await Transaction.findOneAndUpdate({_id:req.body.id},transaction)
-        res.json({success:true,data:transaction})
-    }
+        await Transaction.findOneAndUpdate({_id: req.body.id}, transaction)
+        res.json({success: true, data: transaction, msg: "Successfully Edit Transaction"})
+    },
+    deleteTransaction: async (req, res, next) => {
+        await Transaction.deleteOne({_id: req.body.id})
+        res.json({success: true, msg: "Successfully Delete Transaction"})
+    },
+
 
 }
--- Routes/transaction.router.js
+++ Routes/transaction.router.js
@@ -4,10 +4,12 @@ const transactionController=require('../Controllers/transaction.controller')
 
 router.post('/add',transactionController.addTransaction)
 router.post('/edit',transactionController.editTransaction)
+router.post('/delete',transactionController.deleteTransaction)
 router.get('/',transactionController.listTransaction)
 router.post('/list',transactionController.listTransactionUser)
 router.get('/category',transactionController.listCategory)
 router.get('/category/expense',transactionController.listExpense)
 router.get('/category/income',transactionController.listIncome)
+
 
 module.exports=router;
