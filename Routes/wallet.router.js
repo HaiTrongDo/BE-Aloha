acf3459a8b2d9b870ee874f6b7cee1267e4c4d12
pydev97@gmail.com <leqthinh20530@gmail.com>
2022-08-29 16:30:53 +0700

    create balance

--- Routes/wallet.router.js
+++ Routes/wallet.router.js
@@ -7,6 +7,7 @@ router.post('/render',iconController.renderWallet);
 router.get('',iconController.render);
 router.post('/detail',iconController.getDetail);
 router.post('/update',iconController.updateWallet);
+router.post('/updateBalance',iconController.updateBalance);
 router.post('/delete',iconController.deleteWallet);
 
 module.exports = router;
